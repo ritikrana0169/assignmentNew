8ba3dccf5fa466e4437d03b7ee1621220686e27c
ritik rana <Ritik>
2022-11-29 18:46:41 +0530

    Created Login function

---
+++ login.js
@@ -0,0 +1,15 @@
+let regEmail="ritik@gmail.com";
+let rPass="ritik@123";
+
+let entEmail="ritik@gmail.com";
+let entPass="ritik@123";
+
+if(entEmail==regEmail){
+if(entPass==regPass){
+    console.log("Login Succesfully");
+}else{
+    console.log("Wrong Password");
+}
+}else{
+    console.log("Wrong Email");
+}
